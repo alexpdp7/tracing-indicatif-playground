249bfcfa6b182078c3a83f3537fe27fc881a6e8e
alex <alex@pdp7.net>
2024-04-05 17:57:43 +0200

    Use duct

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
+use std::io::BufRead;
+use std::io::BufReader;
 /// sync/rayon version of https://github.com/emersonford/tracing-indicatif/blob/main/examples/basic.rs
 use std::time::Duration;
 
+use duct::cmd;
 use rand::thread_rng;
 use rand::Rng;
 use rayon::prelude::*;
-use tracing::info;
 use tracing::instrument;
+use tracing::{debug, info};
 use tracing_indicatif::IndicatifLayer;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
@@ -16,6 +19,12 @@ fn do_work(val: u64) -> u64 {
     std::thread::sleep(sleep_time);
 
     info!("doing work for val: {}", val);
+
+    let find = cmd!("bash", "-c", "find / | head -10000");
+    let reader = find.stderr_to_stdout().reader().unwrap();
+    BufReader::new(reader)
+        .lines()
+        .for_each(|l| debug!("{val} {}", l.unwrap()));
 
     let sleep_time =
         thread_rng().gen_range(Duration::from_millis(500)..Duration::from_millis(1000));
